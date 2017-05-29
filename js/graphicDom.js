6256ea4abbf498c5cb0d2107e82897baa763ccde
Кирил Смирнов <ksmirnov@baccasoft.ru>
2017-05-29 21:26:38 +0300

    fix

--- js/graphicDom.js
+++ js/graphicDom.js
@@ -1,34 +1,45 @@
 $(document).ready(function(){
     $('.wheel_dom1').click(function(){
+        $('.wheel_dom .image').css({
+            'display':'none',
+        })
         $('.wheel').css('transform','rotate(45deg)')
-
         $('.wheel_dom1').css('transform','rotate(-45deg)')
-        setTimeout(function(){
-            $('.wheel_dom .image1').css({
-                'display':'block'
-            })
-            $('.wheel_dom .image2').css({
-                'display':'block'
-            })
-            $('.wheel_dom .image3').css({
-                'display':'block'
-            })
-            $('.wheel_dom .image1').animate({
-                'margin-left':'400px'
-            })
-            $('.wheel_dom .image2').animate({
-                'margin-left':'500px',
-            })
-            $('.wheel_dom .image3').animate({
-                'margin-left':'600px',
-            })
-        }, 1000)
     })
     $('.wheel_dom2').click(function(){
+         $('.wheel_dom .image').css({
+            'display':'none',
+        })
         $('.wheel').css('transform','rotate(0deg)')
     })
     $('.wheel_dom3').click(function(){
+         $('.wheel_dom .image').css({
+            'display':'none',
+        })
         $('.wheel').css('transform','rotate(-45deg)')
         $('.wheel_dom3').css('transform','rotate(45deg)')
+    })
+    $('.wheel_dom').click(function(){
+        var dom = $(this);
+        setTimeout(function(){
+            
+            dom.children('.image').css({
+                'display':'block',
+                'margin-left':'0px',
+                'transform':'rotate(20deg)'
+            })
+            dom.children('.image1').animate({
+                'margin-top' : '20px',
+                'margin-left':'400px',
+            })
+            dom.children('.image2').animate({
+                'margin-top' : '110px',
+                'margin-left':'600px',
+            })
+            dom.children('.image3').animate({
+                'margin-top' : '80px',
+                'margin-left':'800px',
+            })
+        }, 1000)
     })
 });
