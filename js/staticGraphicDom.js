01bdd8e05b0cc250995ab8ad07d45c9372b17078
Кирил Смирнов <ksmirnov@baccasoft.ru>
2017-06-02 12:31:52 +0300

    fix user iteraction

--- js/staticGraphicDom.js
+++ js/staticGraphicDom.js
@@ -1,10 +1,38 @@
 $(document).ready(function(){
-    var video = document.getElementById('video');
-    video.addEventListener('canplay', function(e) {
-        this.play();
-    });
-    video.addEventListener('timeupdate', sec13)
     
+    
+    $('.timeline').append(
+        '<button class="timeline1" onclick="clickTimeline1()"></button>'+
+        '<button class="timeline2"></button>'+
+        '<button class="timeline3"></button>'+
+        '<button class="timeline4"></button>'+
+        '<button class="timeline5"></button>'
+    )
+
+    $('.timeline1').click(function(){
+        $(this).addClass('active_btn');
+    });
+
+    $('.timeline .back').click(function(e){
+        var x = e.pageX;
+        var y = e.pageY;
+        
+        $('.timeline button').each(function(i){
+            var btn = $('.timeline button')[i];
+            var xB = $(btn).offset().left;
+            var yB = $(btn).offset().top;
+            var hB = 70;
+            var wB = window.innerWidth/5;
+
+            if(x>xB && y>yB && x<xB+wB && y<yB+hB){
+                allTimelineClick();
+                $('.timeline button').removeClass('active_btn');
+                $(btn).addClass('active_btn');
+                $(btn).click();
+            }
+        })     
+    })
+    $('.timeline1').click();
 })
 
 function sec13(e){
@@ -22,6 +50,22 @@ function sec13(e){
 
 }
 
+function allTimelineClick(){
+    var videos = document.getElementsByTagName('video');
+    for(var i=0; i<videos.length; i++){
+        var video = videos[i];
+        video.pause();
+        video.currentTime = 0;
+        $(video).addClass('hidden_type');
+    }
+}
+
+function clickTimeline1(){   
+    var video = document.getElementById('video');
+    video.play();
+    $(video).removeClass('hidden_type')
+    video.addEventListener('timeupdate', sec13)
+}
 
 function clickButton1(){
     $('.button1').click(function(){
