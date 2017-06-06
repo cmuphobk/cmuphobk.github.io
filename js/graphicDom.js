4a17ace77572610f17bd3115218755e2c4bf6d41
Кирил Смирнов <ksmirnov@baccasoft.ru>
2017-06-06 12:46:37 +0300

    fix

--- js/graphicDom.js
+++ js/graphicDom.js
@@ -247,15 +247,31 @@ function addResizeHandler(arrTouches, element){
     if(oldRad != null){
         var deltaRad = (oldRad - rad);
         var deltaUgol = deltaRad*57.2958;
-        $(element).css({
-            transform: 'rotate('+deltaUgol+')'
-        })
-        /*var deltaUgol = deltaRad*57.2958
-        console.log(deltaUgol)*/
+        if(deltaRad != 0){
+            var elRot = getRotationDegrees($(element));
+            $(element).css({
+                transform: 'rotate('+(elRot+deltaUgol)+'deg)'
+            })
+        }
     }
     
 
     oldRad = rad;
+}
+
+function getRotationDegrees(obj) {
+    var matrix = obj.css("-webkit-transform") ||
+    obj.css("-moz-transform")    ||
+    obj.css("-ms-transform")     ||
+    obj.css("-o-transform")      ||
+    obj.css("transform");
+    if(matrix !== 'none') {
+        var values = matrix.split('(')[1].split(')')[0].split(',');
+        var a = values[0];
+        var b = values[1];
+        var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    } else { var angle = 0; }
+    return (angle < 0) ? angle + 360 : angle;
 }
 
 //просто ансетит кастомные параметры и удаляет листенер mousemove с окна
