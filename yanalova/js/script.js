e211edf1520bf87854fe2dda7b79c6fa3e65bd93
cmuphobk <cmuphob.k@gmail.com>
2018-02-09 15:10:20 +0400

    fix

--- yanalova/js/script.js
+++ yanalova/js/script.js
@@ -1,78 +1,87 @@
 $(document).ready(function(){
 
-var oldScrollY = $(window).scrollTop() + $(window).height();
-window.onscroll = function(e){
-    var nowScrollY = $(window).scrollTop() + $(window).height();
-    var elTop = $('.parallax').offset().top;
-    if(nowScrollY >= elTop){
-        var bPosY = parseFloat($('.parallax').css('background-position-y'));
-        if(nowScrollY > oldScrollY){
-            $('.parallax').css('background-position-y', bPosY - 0.5 + "%");
+    $('.parallax').css('height', $('.left_scene_block').css('height'));
+    setTimeout(function(){
+        $('.parallax').css('height', $('.left_scene_block').css('height'));
+    }, 1);
+    $(window).resize(function(){
+        $('.parallax').css('height', $('.left_scene_block').css('height'));
+    });
+    $('.left_scene_block').css('visibility', 'visible');
+
+    var oldScrollY = $(window).scrollTop() + $(window).height();
+    window.onscroll = function(e){
+        var nowScrollY = $(window).scrollTop() + $(window).height();
+        var elTop = $('.parallax').offset().top;
+        if(nowScrollY >= elTop){
+            var bPosY = parseFloat($('.parallax').css('background-position-y'));
+            if(nowScrollY > oldScrollY){
+                $('.parallax').css('background-position-y', bPosY - 0.5 + "%");
+            }else{
+                $('.parallax').css('background-position-y', bPosY + 0.5 + "%");
+            }
+            
+        }
+        oldScrollY = nowScrollY;
+    }
+        
+    $('#menu span').click(function(){
+        if($('#menu .right_panel ul').height() == 0){
+            $('#menu .right_panel ul').css({
+                "height":"auto"
+            })
         }else{
-            $('.parallax').css('background-position-y', bPosY + 0.5 + "%");
+            $('#menu .right_panel ul').css({
+                "height":"0"
+            })
         }
         
-    }
-    oldScrollY = nowScrollY;
-}
-    
-$('#menu span').click(function(){
-    if($('#menu .right_panel ul').height() == 0){
-        $('#menu .right_panel ul').css({
-            "height":"auto"
-        })
-    }else{
-        $('#menu .right_panel ul').css({
-            "height":"0"
-        })
-    }
-    
-});
-
-
-//ApiKey:0uSqawSFlM35OUbC9svTxA
-function sendEmail(req){
-    emailjs.send("gmail","yanalovamail",req).then(function(response) {
-        
-    }, 
-    function(error) {
-        
     });
-}
-
-//ApiKey:0uSqawSFlM35OUbC9svTxA
-function sendCall(req){
-    emailjs.send("gmail","yanalovacall",req).then(function(response) {
-        
-    }, 
-    function(error) {
-        
-    });
-}
 
 
-var req = {
-    reply_to: 'ksmirnov@baccasoft.ru',
-    copy_to: 'ksmirnov@baccasoft.ru'
-}
-$('#send').click(function(){
-    Object.assign(req,{
-       Name: $('#name').val(),
-       Email: $('#email').val(),
-       Mail: $('#title').val(),
+    //ApiKey:0uSqawSFlM35OUbC9svTxA
+    function sendEmail(req){
+        emailjs.send("gmail","yanalovamail",req).then(function(response) {
+            
+        }, 
+        function(error) {
+            
+        });
+    }
+
+    //ApiKey:0uSqawSFlM35OUbC9svTxA
+    function sendCall(req){
+        emailjs.send("gmail","yanalovacall",req).then(function(response) {
+            
+        }, 
+        function(error) {
+            
+        });
+    }
+
+
+    var req = {
+        reply_to: 'ksmirnov@baccasoft.ru',
+        copy_to: 'ksmirnov@baccasoft.ru'
+    }
+    $('#send').click(function(){
+        Object.assign(req,{
+        Name: $('#name').val(),
+        Email: $('#email').val(),
+        Mail: $('#title').val(),
+        })
+        sendEmail(req);
     })
-    sendEmail(req);
-})
 
 
-$('#call').click(function(){
-    Object.assign(req,{
-       Name: $('#namecall').val(),
-       Email: $('#emailcall').val(),
-       Phone: $('#phone').val(),
+    $('#call').click(function(){
+        Object.assign(req,{
+        Name: $('#namecall').val(),
+        Email: $('#emailcall').val(),
+        Phone: $('#phone').val(),
+        })
+        sendCall(req);
     })
-    sendCall(req);
-})
 
 });
 
